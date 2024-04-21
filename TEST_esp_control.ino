2b5e4bd51ccfe7ad5d2477be7de9c3583b5fa967
RinAka615 <136349652+RinAka615@users.noreply.github.com>
2024-04-22 01:56:12 +0700

    Update TEST_esp_control.ino
    Signed-off-by: RinAka615 <136349652+RinAka615@users.noreply.github.com>

--- TEST_esp_control.ino
+++ TEST_esp_control.ino
@@ -1,14 +1,29 @@
 #include <WiFi.h>
 #include <esp_now.h>
 
+#define Forward 26
+#define Backward 27
+#define Right 32
+#define Left 33
+#define Up 34 // tam giác
+#define Drop 35 // tròn
+#define Down 36 // X
+#define Hold 39 // vuông
+
 // Địa chỉ MAC của ESP nhận
 uint8_t receiverAddress[] = {1, 1, 1, 1, 1, 1};
 
 // Thiết lập tín hiệu điều khiển
 struct ControlSignal {
-  
+  int forward;
+  int backward;
+  int turn_left;
+  int turn_right;
+  int up;
+  int down;
+  int hold;
+  int drop;
 };
-
 
 // Hàm gửi tín hiệu điều khiển
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
@@ -25,6 +40,17 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
+
+  pinMode(Forward, INPUT);
+  pinMode(Backward, INPUT);
+  pinMode(Left, INPUT);
+  pinMode(Right, INPUT);
+  pinMode(Up, INPUT);
+  pinMode(Down, INPUT);
+  pinMode(Hold, INPUT);
+  pinMode(Drop, INPUT);
+
+  
 
   // Khởi tạo ESP Now
   if (esp_now_init() != ESP_OK) {
@@ -52,6 +78,14 @@ void loop() {
   ControlSignal Signal;
 
   // Nhập tín hiệu từ tay cầm
+  Signal.forward = digitalRead(Forward);
+  Signal.backward = digitalRead(Backward);
+  Signal.turn_right = digitalRead(Right);
+  Signal.turn_left = digitalRead(Left);
+  Signal.up = digitalRead(Up);
+  Signal.drop = digitalRead(Drop);
+  Signal.down = digitalRead(Down);
+  Signal.hold = digitalRead(Hold);
 
   // Truyền dữ liệu
   esp_err_t result = esp_now_send(
@@ -66,5 +100,5 @@ void loop() {
     Serial.println("Error sending the data");
   }
 
-  delay(100); // Đảm bảo không gửi dữ liệu quá nhanh
+  delay(100);
 }

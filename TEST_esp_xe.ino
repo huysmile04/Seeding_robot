0dcd0a1bd5af2108c6f094661b509a9f9ef6863d
RinAka615 <136349652+RinAka615@users.noreply.github.com>
2024-04-22 01:56:31 +0700

    Update TEST_esp_xe.ino
    Signed-off-by: RinAka615 <136349652+RinAka615@users.noreply.github.com>

--- TEST_esp_xe.ino
+++ TEST_esp_xe.ino
@@ -1,13 +1,78 @@
 #include <WiFi.h>
 #include <esp_now.h>
+#include <ESP32Servo.h>
 
-// Thiết lập setup
+// Thiết lập 
+#define MOTOR_A_IN1 26
+#define MOTOR_A_IN2 27
+#define MOTOR_A_EN 14
+#define MOTOR_B_IN1 32
+#define MOTOR_B_IN2 33
+#define MOTOR_B_EN 15
+#define SERVO0 12
+#define SERVO1 13
 
+
+static  Servo Servo_0; // lên xuống
+static  Servo Servo_1; // gắp thả
 
 // Thiết lập tín hiệu điều khiển
 struct ControlSignal {
-
+  int forward;
+  int backward;
+  int turn_left;
+  int turn_right;
+  int up;
+  int down;
+  int hold;
+  int drop;
 };
+
+void forward() {
+  digitalWrite(MOTOR_A_IN1, HIGH);
+  digitalWrite(MOTOR_A_IN2, LOW);
+  digitalWrite(MOTOR_B_IN1, HIGH);
+  digitalWrite(MOTOR_B_IN2, LOW);
+  analogWrite(MOTOR_A_EN, 255);
+  analogWrite(MOTOR_B_EN, 255);
+}
+
+void backward() {
+  digitalWrite(MOTOR_A_IN1, LOW);
+  digitalWrite(MOTOR_A_IN2, HIGH);
+  digitalWrite(MOTOR_B_IN1, LOW);
+  digitalWrite(MOTOR_B_IN2, HIGH);
+  analogWrite(MOTOR_A_EN, 255);
+  analogWrite(MOTOR_B_EN, 255);
+}
+
+void turn_left() {
+  digitalWrite(MOTOR_A_IN1, LOW);
+  digitalWrite(MOTOR_A_IN2, LOW);
+  digitalWrite(MOTOR_B_IN1, HIGH);
+  digitalWrite(MOTOR_B_IN2, LOW);
+  analogWrite(MOTOR_A_EN, 255);
+  analogWrite(MOTOR_B_EN, 255);
+
+}
+
+void turn_right() {
+  digitalWrite(MOTOR_A_IN1, HIGH);
+  digitalWrite(MOTOR_A_IN2, LOW);
+  digitalWrite(MOTOR_B_IN1, LOW);
+  digitalWrite(MOTOR_B_IN2, LOW);
+  analogWrite(MOTOR_A_EN, 255);
+  analogWrite(MOTOR_B_EN, 255);
+
+}
+
+void stop() {
+  digitalWrite(MOTOR_A_IN1, LOW);
+  digitalWrite(MOTOR_A_IN2, LOW);
+  digitalWrite(MOTOR_B_IN1, LOW);
+  digitalWrite(MOTOR_B_IN2, LOW);
+
+}
 
 ControlSignal Signal;
 
@@ -17,8 +82,41 @@ void onDataReceived(const uint8_t *mac, const uint8_t *data, int len) {
     ControlSignal controlSignal;
     memcpy(&Signal, data, sizeof(Signal));
 
-    // Điều khiển xe dựa trên dữ liệu nhận được
+    if (Signal.forward == HIGH) {
+      forward();
+    }
 
+    if (Signal.backward == HIGH) {
+      backward();
+    }
+
+    if (Signal.turn_left == HIGH) {
+      turn_left();
+    }
+
+    if (Signal.turn_right == HIGH) {
+      turn_right();
+    }
+
+    if (Signal.forward == LOW && Signal.backward == LOW && Signal.turn_left == LOW && Signal.turn_right == LOW) {
+      stop();
+    }
+
+    if(Signal.up == HIGH) {
+      Servo_0.write(90);
+    }
+
+    if (Signal.down == HIGH) {
+      Servo_0.write(0);
+    } 
+
+    if (Signal.hold == HIGH) {
+      Servo_1.write(80);
+    }
+
+    if (Signal.drop == HIGH) {
+      Servo_1.write(0);
+    }
   }
 }
 
@@ -33,6 +131,19 @@ void setup() {
 
   // Đăng ký hàm xử lý dữ liệu nhận được
   esp_now_register_recv_cb(onDataReceived);
+
+  pinMode(MOTOR_A_IN1, OUTPUT);
+  pinMode(MOTOR_A_IN2, OUTPUT);
+  pinMode(MOTOR_B_IN1, OUTPUT);
+  pinMode(MOTOR_B_IN2, OUTPUT);
+  pinMode(MOTOR_A_EN, OUTPUT);
+  pinMode(MOTOR_B_EN, OUTPUT);
+
+  Servo_0.attach(SERVO0);
+  Servo_1.attach(SERVO1);
+  Servo_0.write(0);
+  Servo_1.write(0);
+
 }
 
 void loop() {
